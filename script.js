e24caa6e2390c6f0e11d01d472a3c3fa9430cfb2
William Alves <willshosters@gmail.com>
2024-08-24 00:47:45 -0300

    header com javascript adicionado

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o header:', error));
+});
